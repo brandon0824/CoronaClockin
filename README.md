77a37fd89646ae7ba12ffa4eaf8c01bfe389d965
brandon0824 <446038117@qq.com>
2020-10-18 09:58:50 +0800

    initial

---
+++ README.md
@@ -0,0 +1,26 @@
+# Questionnaire for Coronavirus by Selenium
+
+## Install
+
+### Download python and selenium
+
+```bash
+pip install selenium
+```
+
+### Config Chrome Driver in PC
+
+## Configure
+
+Config your username and password in info.json
+
+```bash
+{"username": 123456,"password":"123456"}
+```
+
+## Run
+
+```bash
+python clockin.py
+```
+
---
+++ clockin.py
@@ -0,0 +1,59 @@
+from selenium import webdriver
+import time as tm
+import random
+import json
+
+with open('info.json', 'r') as myfile:
+    data=myfile.read()
+
+obj = json.loads(data)
+# print("username: " + str(obj['username']))
+# print("password:" + str(obj['password']))
+myusername = str(obj['username'])
+mypassword = str(obj['password'])
+# print(myusername, mypassword)
+
+login_url = 'https://jkxxcj.zjhu.edu.cn/login.html'
+
+driver = webdriver.Chrome()
+driver.get(login_url)
+driver.maximize_window()
+tm.sleep(3)
+
+username = driver.find_element_by_id('zgh')
+username.send_keys(myusername)
+
+password = driver.find_element_by_id('mm')
+password.send_keys(mypassword)
+
+button = driver.find_element_by_id('loginBtn')
+button.click()
+
+tm.sleep(3)
+healthReport = driver.find_element_by_link_text('健康报告')
+healthReport.click()
+
+a = random.uniform(36.6, 37.4)
+b = random.uniform(36.6, 37.4)
+a1 = round(a, 1)
+b1 = round(b, 1)
+# print(a1, b1)
+mornTemp = str(a1)
+nightTemp = str(b1)
+# print(mornTemp, nightTemp)
+
+tm.sleep(3)
+inputMorn = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/form/div[19]/div/textarea")
+inputMorn.clear()
+inputMorn.send_keys(mornTemp)
+
+tm.sleep(3)
+inputNight = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/form/div[20]/div/textarea")
+inputNight.clear()
+inputNight.send_keys(nightTemp)
+
+# tm.sleep(3)
+# confirm = driver.find_element_by_id('saveBtn')
+# confirm.click()
+
+# driver.close()
