80b395c84aae3ae104d08fafe38f43d23d70a020
brandon0824 <446038117@qq.com>
2021-01-19 19:22:55 +0800

    enhance account

--- clockin.py
+++ clockin.py
@@ -4,63 +4,81 @@ import time as tm
 import random
 import json
 
-print("-----Auto Fill Form Starting-----")
-with open('info.json', 'r') as myfile:
-    data=myfile.read()
+def readJson():
 
-obj = json.loads(data)
-# print("username: " + str(obj['username']))
-# print("password:" + str(obj['password']))
-myusername = str(obj['username'])
-mypassword = str(obj['password'])
+    username = []
+    password = []
 
-print("Your username is : " + myusername)
-print("Your password is : " + mypassword)
+    with open('C:/Users/Brandon/Desktop/data.json', 'r') as myfile:
+        data=myfile.read()
 
-login_url = 'https://jkxxcj.zjhu.edu.cn/login.html'
+    obj = json.loads(data)
+    member = obj['members']
+    print(json.dumps(member, indent=4, ensure_ascii=False))
 
-driver = webdriver.Chrome()
-driver.get(login_url)
-driver.maximize_window()
-tm.sleep(3)
+    for i in range(len(member)):
+        # print(json.dumps(member[i], indent=4, ensure_ascii=False))
+        un = member[i]['username']
+        pwd = member[i]['password']
+        username.append(un)
+        password.append(pwd)
+    print(username)
+    print(password)
 
-username = driver.find_element_by_id('zgh')
-username.send_keys(myusername)
+    return username,password
 
-password = driver.find_element_by_id('mm')
-password.send_keys(mypassword)
+def clockin(myusername, mypassword):
+    login_url = 'https://jkxxcj.zjhu.edu.cn/login.html'
 
-button = driver.find_element_by_id('loginBtn')
-button.click()
+    driver = webdriver.Chrome()
+    driver.get(login_url)
+    driver.maximize_window()
+    tm.sleep(3)
 
-tm.sleep(3)
-healthReport = driver.find_element_by_link_text('健康报告')
-healthReport.click()
+    username = driver.find_element_by_id('zgh')
+    username.send_keys(myusername)
 
-a = random.uniform(36.6, 37.4)
-b = random.uniform(36.6, 37.4)
-a1 = round(a, 1)
-b1 = round(b, 1)
-# print(a1, b1)
-mornTemp = str(a1)
-nightTemp = str(b1)
-print("Random morning Temperature : " + mornTemp)
-print("Random night Temperature : " + nightTemp)
+    password = driver.find_element_by_id('mm')
+    password.send_keys(mypassword)
 
-tm.sleep(3)
-inputMorn = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/form/div[19]/div/textarea")
-inputMorn.clear()
-inputMorn.send_keys(mornTemp)
+    button = driver.find_element_by_id('loginBtn')
+    button.click()
 
-tm.sleep(3)
-inputNight = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/form/div[20]/div/textarea")
-inputNight.clear()
-inputNight.send_keys(nightTemp)
+    tm.sleep(3)
+    healthReport = driver.find_element_by_link_text('健康报告')
+    healthReport.click()
 
-tm.sleep(3)
-confirm = driver.find_element_by_id('saveBtn')
-confirm.click()
+    a = random.uniform(36.6, 37.4)
+    b = random.uniform(36.6, 37.4)
+    a1 = round(a, 1)
+    b1 = round(b, 1)
+    # print(a1, b1)
+    mornTemp = str(a1)
+    nightTemp = str(b1)
+    print("Random morning Temperature : " + mornTemp)
+    print("Random night Temperature : " + nightTemp)
 
-tm.sleep(3)
-driver.close()
-print("-----Auto Fill Form Finished-----")
+    tm.sleep(3)
+    inputMorn = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/form/div[19]/div/textarea")
+    inputMorn.clear()
+    inputMorn.send_keys(mornTemp)
+
+    tm.sleep(3)
+    inputNight = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/form/div[20]/div/textarea")
+    inputNight.clear()
+    inputNight.send_keys(nightTemp)
+
+    tm.sleep(3)
+    confirm = driver.find_element_by_id('saveBtn')
+    confirm.click()
+
+    tm.sleep(3)
+    driver.close()
+
+
+if __name__ == '__main__':
+    print("-----Auto Fill Form Starting-----")
+    uname, pwd = readJson()
+    for i in range(len(uname)):
+        clockin(uname[i], pwd[i])
+    print("-----Auto Fill Form Finished-----")
