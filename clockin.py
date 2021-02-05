a25fed6649e114e523434ac39257a8d516c12914
brandon0824 <446038117@qq.com>
2021-02-05 15:53:04 +0800

    add check()

--- clockin.py
+++ clockin.py
@@ -14,7 +14,7 @@ def readJson():
 
     obj = json.loads(data)
     member = obj['members']
-    print(json.dumps(member, indent=4, ensure_ascii=False))
+    # print(json.dumps(member, indent=4, ensure_ascii=False))
 
     for i in range(len(member)):
         # print(json.dumps(member[i], indent=4, ensure_ascii=False))
@@ -22,13 +22,14 @@ def readJson():
         pwd = member[i]['password']
         username.append(un)
         password.append(pwd)
-    print(username)
-    print(password)
+    # print(username)
+    # print(password)
 
     return username,password
 
 def clockin(myusername, mypassword):
     login_url = 'https://jkxxcj.zjhu.edu.cn/login.html'
+    check_url = 'https://jkxxcj.zjhu.edu.cn/historyList.html'
 
     driver = webdriver.Chrome()
     driver.get(login_url)
@@ -43,51 +44,75 @@ def clockin(myusername, mypassword):
 
     button = driver.find_element_by_id('loginBtn')
     button.click()
-
     tm.sleep(3)
-    healthReport = driver.find_element_by_link_text('健康报告')
-    healthReport.click()
 
-    a = random.uniform(36.6, 37.4)
-    b = random.uniform(36.6, 37.4)
-    a1 = round(a, 1)
-    b1 = round(b, 1)
-    # print(a1, b1)
-    mornTemp = str(a1)
-    nightTemp = str(b1)
-    print("Random morning Temperature : " + mornTemp)
-    print("Random night Temperature : " + nightTemp)
-
-    tm.sleep(3)
-    inputMorn = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/form/div[19]/div/textarea")
-    inputMorn.clear()
-    inputMorn.send_keys(mornTemp)
-
-    tm.sleep(3)
-    inputNight = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/form/div[20]/div/textarea")
-    inputNight.clear()
-    inputNight.send_keys(nightTemp)
-
-    tm.sleep(3)
-    confirm = driver.find_element_by_id('saveBtn')
-    confirm.click()
-
-    tm.sleep(3)
-    check_url = 'https://jkxxcj.zjhu.edu.cn/historyList.html'
     driver.get(check_url)
     tm.sleep(3)
 
     obj = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/table/tbody/tr[1]/td[2]/span")
     state = obj.text
+    # print(f'state的值是{state}')
     # type(state) = 'str' 2state:已填写/未填写
-    # print(state)
     if state == '已填写':
-        print(f'The account of {myusername} signed succeed!')
+        print(f'The account of {myusername} has been signed succeed!\n')
+        driver.quit()
+        return state
     else:
-        print(f'The account of {myusername} signed failed!')
+        driver.get(login_url)
+        tm.sleep(3)
 
-    tm.sleep(3)
-    driver.close()
+        username = driver.find_element_by_id('zgh')
+        username.send_keys(myusername)
+
+        password = driver.find_element_by_id('mm')
+        password.send_keys(mypassword)
+
+        button = driver.find_element_by_id('loginBtn')
+        button.click()
+
+        healthReport = driver.find_element_by_link_text('健康报告')
+        healthReport.click()
+
+        a = random.uniform(36.6, 37.4)
+        b = random.uniform(36.6, 37.4)
+        a1 = round(a, 1)
+        b1 = round(b, 1)
+        # print(a1, b1)
+        mornTemp = str(a1)
+        nightTemp = str(b1)
+        print("Random morning Temperature : " + mornTemp)
+        print("Random night Temperature : " + nightTemp)
+
+        tm.sleep(3)
+        inputMorn = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/form/div[19]/div/textarea")
+        inputMorn.clear()
+        inputMorn.send_keys(mornTemp)
+
+        tm.sleep(3)
+        inputNight = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/form/div[20]/div/textarea")
+        inputNight.clear()
+        inputNight.send_keys(nightTemp)
+
+        tm.sleep(3)
+        confirm = driver.find_element_by_id('saveBtn')
+        confirm.click()
+
+        tm.sleep(3)
+        check_url = 'https://jkxxcj.zjhu.edu.cn/historyList.html'
+        driver.get(check_url)
+        tm.sleep(3)
+
+        obj = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/table/tbody/tr[1]/td[2]/span")
+        state = obj.text
+        # type(state) = 'str' 2state:已填写/未填写
+        # print(state)
+        if state == '已填写':
+            print(f'The account of {myusername} signed succeed!')
+        else:
+            print(f'The account of {myusername} signed failed!')
+
+        tm.sleep(3)
+        driver.quit()
 
 
 
